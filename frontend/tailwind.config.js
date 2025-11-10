c878a26e79c1e62df278408ea2363b4d14626de8
Shivang Upadhyay <shivang14071993@gmail.com>
2025-11-10 16:44:42 +0530

    hero section done

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        'course-details-heading-small' : ['26px', '36px'],
+        'course-details-heading-large' : ['36px', '44px']
+      }
+    },
   },
   plugins: [],
 }
