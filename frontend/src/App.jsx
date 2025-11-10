05754fea3f5bf4714cbbac709f1dcc7421fc0c43
Shivang Upadhyay <shivang14071993@gmail.com>
2025-11-10 16:14:50 +0530

    Navbar with clerk implementation done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, useMatch } from "react-router-dom"
 import Home from "./pages/student/Home"
 import CourseList from "./pages/student/CourseList"
 import CourseDetails from "./pages/student/CourseDetails"
@@ -6,27 +6,32 @@ import MyEnrollment from "./pages/student/MyEnrollment"
 import Player from "./pages/student/Player"
 import Loading from "./components/student/Loading"
 import Educator from "./pages/educator/Educator"
-import Dasborad from "./pages/educator/Dasborad"
+import Dashboard from "./pages/educator/Dashboard"
 import AddCourse from "./pages/educator/AddCourse"
 import MyCourses from "./pages/educator/MyCourses"
 import StudentEnrolled from "./pages/educator/StudentEnrolled"
+import Navbar from "./components/student/Navbar"
 
 function App() {
+
+  const isEducatorRoute = useMatch('/educator/*')
  return(
-  <div>
+  <div className="text-default min-h-screen bg-white">
+    {!isEducatorRoute && <Navbar/>}
     <Routes>
     <Route path="/" element={<Home/>}/>
     <Route path="/course-list" element={<CourseList/>}/>
     <Route path="/course-list/:input" element={<CourseList/>}/>
     <Route path="/course/:id" element={<CourseDetails/>}/>
-    <Route path="/my-enrollment" element={<MyEnrollment/>}/>
+    <Route path="/my-enrollments" element={<MyEnrollment/>}/>
     <Route path="/player/:courseId" element={<Player/>}/>
     <Route path="/loading/:path" element={<Loading/>}/>
-    <Route path="/educator" element={<Educator/>}/>
-         <Route path="/educator" element={<Dasborad/>}/>
-         <Route path="/add-course" element={<AddCourse/>}/>
-         <Route path="/my-course" element= {<MyCourses/>}/>
-         <Route path="/student-enrolled" element={<StudentEnrolled/>}/>
+    <Route path="/educator" element={<Educator/>}>
+         <Route path="educator" element={<Dashboard/>}/>
+         <Route path="add-course" element={<AddCourse/>}/>
+         <Route path="my-course" element={<MyCourses/>}/>
+         <Route path="student-enrolled" element={<StudentEnrolled/>}/>
+    </Route>
     </Routes>
   </div>
  )
