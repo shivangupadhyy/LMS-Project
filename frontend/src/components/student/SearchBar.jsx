74cd882d9f6204b9cf4fd0f685a4673509523802
Shivang Upadhyay <shivang14071993@gmail.com>
2025-11-11 23:27:16 +0530

    Search button done with functionality

--- frontend/src/components/student/SearchBar.jsx
+++ frontend/src/components/student/SearchBar.jsx
@@ -15,10 +15,10 @@ const SearchBar = ({data}) => {
     <div>
       <form onSubmit={onSearchHandler} className='max-w-xl w-full md:h-14 h-12 flex items-center bg-white border border-gray-500/20 rounded'>
         <img src={assets.search_icon} alt="search_icon" className='md:w-auto w-10 px-3' />
-        {/* make input take remaining space */}
+        
         <input onChange={e => setInput(e.target.value)} value={input} 
          type='text' placeholder='Search for courses' className='flex-1 h-full outline-none text-gray-500/80 px-2'/>
-        {/* give button a predictable width and prevent shrinking */}
+       
         <button type='submit' className='bg-blue-600 rounded text-white md:w-40 w-28 md:py-3 py-2 mx-1 flex-shrink-0'>Search</button>
       </form>
     </div>
