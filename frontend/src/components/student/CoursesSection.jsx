571b1598967de0af316da29ca2bccfd08eadb2ab
Shivang Upadhyay <shivang14071993@gmail.com>
2025-11-13 14:40:03 +0530

    Course Section Ready

--- frontend/src/components/student/CoursesSection.jsx
+++ frontend/src/components/student/CoursesSection.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import { AppContext } from '../../context/AppContext'
+import CourseCard from './CourseCard'
 
 const CoursesSection = () => {
+
+  const {allCourses} = useContext(AppContext)
   return (
     <div className='py-16 md:px-40 px-8'>
      <h2 className='text-3xl font-medium text-gray-800'>Learn from the best</h2>
      <p className='text-sm md:text-base mt-3 text-gray-500'>Discover out top-rated courses across various categories. From coding and design to business and wellness, our courses are crafted to deliver results.</p>
 
-
+     <div>
+      {allCourses.slice(0.4).map((course, index)=><CourseCard key={index} course={course}/>)}
+     </div>
 
 
      <Link to={'/course-list'} onClick={()=> scrollTo(0,0)}
--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,11 +1,23 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+import { dummyCourses } from "../assets/assets";
 
 export const AppContext = createContext();
 
 export const AppContextProvider = (props) =>{
 
-    const value = {
+    const currency = import.meta.env.VITE_CURRENCY
 
+    const [allCourses, setAllCourses] = useState([])
+    const fetchAllCourses = async()=>{
+        setAllCourses(dummyCourses)
+    }
+
+
+    useEffect(()=>{
+        fetchAllCourses()
+    },[])
+    const value = {
+        currency, allCourses
     }
     return(
         <AppContext.Provider value={value}>
