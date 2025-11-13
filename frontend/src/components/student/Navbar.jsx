b2df93d9501daabbcbe709fb0f41adc33b617f2e
Shivang Upadhyay <shivang14071993@gmail.com>
2025-11-13 15:23:31 +0530

    Card section upadate

--- frontend/src/components/student/Navbar.jsx
+++ frontend/src/components/student/Navbar.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { assets } from '../../assets/assets'
 import { Link } from 'react-router-dom'
 import {SignIn, useClerk, UserButton, useUser} from '@clerk/clerk-react'
+import { AppContext } from '../../context/AppContext'
 
 const Navbar = () => {
 
@@ -9,15 +10,18 @@ const Navbar = () => {
 
   const {openSignIn} = useClerk()
   const {user} = useUser()
+
+  const {navigate} = useContext(AppContext);
+
   return (
     <div className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${isCoursesListPage ? 'bg-white' : 'bg-cyan-100/70'} `}>
-      <img src={assets.logo} alt='Logo' className='w-28 lg:w-32 cursor-pointer'/>
+      <img onClick={()=> navigate('/')} src={assets.logo} alt='Logo' className='w-28 lg:w-32 cursor-pointer'/>
       <div className='hidden md:flex items-center gap-5 text-gray-500'>
         <div className='flex items-center gap-5'>
           {user && 
           <>  
             <button>Become Educator</button>
-          <Link to='//my-enrollments'>My Enrollments</Link>
+          <Link to='/my-enrollment'>My Enrollments</Link>
           </>
           }
         </div>
@@ -30,7 +34,7 @@ const Navbar = () => {
           {user && 
           <>
           <button>Become Educator</button>
-          <Link to='//my-enrollments'>My Enrollments</Link>
+          <Link to='/my-enrollment'>My Enrollments</Link>
           </>
           }
         </div>
--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,28 +1,39 @@
 import { createContext, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
 export const AppContext = createContext();
 
-export const AppContextProvider = (props) =>{
+export const AppContextProvider = (props) => {
+  const currency = import.meta.env.VITE_CURRENCY;
+  const navigate  = useNavigate()
 
-    const currency = import.meta.env.VITE_CURRENCY
+  const [allCourses, setAllCourses] = useState([]);
 
-    const [allCourses, setAllCourses] = useState([])
-    const fetchAllCourses = async()=>{
-        setAllCourses(dummyCourses)
+  const [isEducator, setIsEducator] = useState(true);
+  const fetchAllCourses = async () => {
+    setAllCourses(dummyCourses);
+  };
+
+  const calculateRating  = (course) =>{
+    if(!course.courseRatings || course.courseRatings.length === 0){
+      return 0;
     }
+    let totalRating = 0
+    course.courseRatings.forEach(rating => {
+      totalRating += rating.rating
+    })
+    return totalRating / course.courseRatings.length
+  }
 
-
-    useEffect(()=>{
-        fetchAllCourses()
-    },[])
-    const value = {
-        currency, allCourses
-    }
-    return(
-        <AppContext.Provider value={value}>
-            {props.children}
-        </AppContext.Provider>
-    )
-}
-
+  useEffect(() => {
+    fetchAllCourses();
+  }, []);
+  const value = {
+    currency,
+    allCourses,navigate, calculateRating
+  };
+  return (
+    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
+  );
+};
