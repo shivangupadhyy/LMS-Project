abe196568a877ac5f19e94f1b82c918abefbf78d
Shivang Upadhyay <shivang14071993@gmail.com>
2025-11-13 16:27:06 +0530

    Completed the footer

--- frontend/src/components/student/TestimonialSection.jsx
+++ frontend/src/components/student/TestimonialSection.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import { assets, dummyTestimonial } from '../../assets/assets'
 
 const TestimonialSection = () => {
   return (
-    <div>
-      <h1>TestimonialScetion</h1>
+    <div className='pb-14 px-8 md:px-0'>
+      <h2 className='text-3xl font-medium text-gray-800'>Testimonials</h2>
+      <p>hear from our learners as they share their journeys of transformation, success, and how our<br/> platform has made a difference in their lives. </p>
+      <div className='grid grid-cols-auto gap-8 mt-14'>
+        {dummyTestimonial.map((Testimonial, index)=>(
+          <div key={index} className='text-sm text-left border border-gray-500/30 pb-6 rounded-lg bg-white show-[0px_4px_15px_0px] shadow-black/5 overflow-hidden'>
+            <div className='flex items-center gap-4 px-5 py-4 bg-gray-500/10'>
+              <img className='h-12 w-12 rounded-full' src={Testimonial.image} alt={Testimonial.name} />
+              <div>
+                <h1 className='text-lg font-medium text-gary-800'>{Testimonial.name}</h1>
+                <p className='text-gray-800/80'>{Testimonial.role}</p>
+                </div>
+            </div>
+             <div className='p-5 pb-7'>
+                  <div className='flex gap-0.5'>
+                    {[...Array(5)].map((_, i) =>(
+                      <img className='h-5' key={i} src = {i < Math.floor(Testimonial.rating) ? assets.star : assets.star_blank} alt='star' />
+                    ))}
+                  </div>
+                  <p className='text-gray-500 mt-5'>{Testimonial.feedback}</p>
+                </div>
+                <a href='#' className='text-blue-500 underline px-5'></a>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
