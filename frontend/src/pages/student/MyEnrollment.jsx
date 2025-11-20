e97e9d1d7d9f3ccf110c91c5c46180a3ce62822f
Shivang Upadhyay <shivang14071993@gmail.com>
2025-11-21 00:25:05 +0530

    My-enrollment section complted

--- frontend/src/pages/student/MyEnrollment.jsx
+++ frontend/src/pages/student/MyEnrollment.jsx
@@ -1,9 +1,11 @@
 import React, { useContext, useState } from 'react'
 import { AppContext } from '../../context/AppContext'
+import {Line} from 'rc-progress'
+import Footer from '../../components/student/Footer'
 
 const MyEnrollment = () => {
 
-  const {enrolledCourses, calculateCourseDuration}  = useContext(AppContext)
+  const {enrolledCourses, calculateCourseDuration, navigate}  = useContext(AppContext)
   const [progressArray, setProgressArray] = useState([
     {lectureCompleted : 2, tolalLectures: 4},
     {lectureCompleted : 1, tolalLectures: 5},
@@ -38,24 +40,27 @@ const MyEnrollment = () => {
             <tr key={index} className='border-b border-gray-500/20'>
               <td className='md:px-4 pl-2 md:pl-4 py-3 flex items-center space-x-3'>
                 <img src={course.courseThumbnail} alt="" className='w-14 sm:w-24 md:w-28' />
-                <div className='flex'>
+                <div className='flex-1'>
                   <p className='md-1 max-sm:text-sm'>{course.courseTitle}</p>
+                  <Line strokeWidth={2} percent={progressArray[index] ? (progressArray[index].lectureCompleted * 100) / progressArray[index].tolalLectures : 0}  className='bg-gray-300 rounded-full'/>
                 </div>
               </td>
               <td className='px-4 py-3 max-sm:hidden'>
                 {calculateCourseDuration(course)}
               </td>
               <td className='px-4 py-3 max-sm:hidden'>
-                4 / 10 <span>Lectures</span>
+                {progressArray[index] && `${progressArray[index].lectureCompleted} / ${progressArray[index].tolalLectures}`} <span>Lectures</span>
               </td>
               <td className='px-4 py-3 max-sm:text-right'>
-                <button className='px-3 sm:px-5 py-1.5 sm:py-2 bg-blue-600 max-sm:text-xs text-white'>On going</button>
+                <button onClick={()=> navigate('/player/' + course._id)} className='px-3 sm:px-5 py-1.5 sm:py-2 bg-blue-600 max-sm:text-xs text-white'>
+                  {progressArray[index] && progressArray[index].lectureCompleted / progressArray[index].tolalLectures ==1 ? 'Completed' : 'On Going'}</button>
               </td>
             </tr>
           ))}
         </tbody>
       </table>
     </div>
+    <Footer/>
     </>
   )
 }
